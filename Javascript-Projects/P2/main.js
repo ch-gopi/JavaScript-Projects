52a087d567684e7370b0072b60e556667a43fd03
ch-gopi <58359910+ch-gopi@users.noreply.github.com>
2023-09-12 08:38:01 +0530

    Add files via upload

---
+++ Javascript-Projects/P2/main.js
@@ -0,0 +1,7 @@
+
+let firstname=prompt('please enter your first name')
+let lastname=prompt('please enter your last name')
+let age=parseInt(prompt('please enter your age')) 
+age=age+2
+console.log('My name is' + firstname+' '+lastname)
+console.log('Age is'+ age)
