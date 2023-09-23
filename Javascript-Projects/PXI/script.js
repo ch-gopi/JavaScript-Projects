863adda8e1d2ea2502c20839f553c271849ea50e
ch-gopi <58359910+ch-gopi@users.noreply.github.com>
2023-09-24 02:33:59 +0530

    Add files via upload

---
+++ Javascript-Projects/PXI/script.js
@@ -0,0 +1,12 @@
+
+console.log('can you see me');  
+window.alert('Iam Gopi');
+document.write('Have a nice day!');
+document.getElementById("header").innerHTML = "<h1>Welcome to JavaScript</h1>";
+function img(){
+    console.log('clicked');
+    var image=document.getElementById("image");
+    image.src="merns.jpeg";
+
+}
+
