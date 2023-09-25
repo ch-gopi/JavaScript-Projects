f55d7db83cbbfddabec2de6d0d1cdc172e3d61cb
ch-gopi <58359910+ch-gopi@users.noreply.github.com>
2023-09-25 09:51:20 +0530

    Update script.js

--- Javascript-Projects/PXV/script.js
+++ Javascript-Projects/PXV/script.js
@@ -1,31 +1,31 @@
-    document.write(1);
-	document.write('<br>');  /// this is just a line break
-	document.write(1.5);
-	document.write('<br>');	/// this is just a line break
-	///Strings
-	document.write('Hello');
-	document.write('<br>');	/// this is just a line break
-	document.write("Hello");
-	document.write('<br>');	/// this is just a line break
+document.write(1);
+document.write('<br>');  /// this is just a line break
+document.write(1.5);
+document.write('<br>');	/// this is just a line break
+///Strings
+document.write('Hello');
+document.write('<br>');	/// this is just a line break
+document.write("Hello");
+document.write('<br>');	/// this is just a line break
 
-	///Variables
-	var myHolder = 6;
-	document.write(myHolder );
-	document.write('<br>');	/// this is just a line break
-	var myText = "hello";
-	document.write(myText);
-	document.write('<br>');	/// this is just a line break
+///Variables
+var myHolder = 6;
+document.write(myHolder );
+document.write('<br>');	/// this is just a line break
+var myText = "hello";
+document.write(myText);
+document.write('<br>');	/// this is just a line break
 
-	///Expressions
-	document.write(myHolder + myHolder );
-	document.write('<br>');	/// this is just a line break
-	document.write(myHolder * myHolder );
-	document.write('<br>');	/// this is just a line break
-	document.write(myText + ' ' + myText + ' ' + myHolder);
-	document.write('<br>');	/// this is just a line break
+///Expressions
+document.write(myHolder + myHolder );
+document.write('<br>');	/// this is just a line break
+document.write(myHolder * myHolder );
+document.write('<br>');	/// this is just a line break
+document.write(myText + ' ' + myText + ' ' + myHolder);
+document.write('<br>');	/// this is just a line break
 
-    	/*
-	*********
+/*
+*********
 Typically for JavaScript when we combine words we should keep the first letter lowercase of the first word and then uppercase for the second.
 
 newWord
@@ -33,65 +33,65 @@ firstName
 lastName
 
 Don't use numbers as the first character, also remember its case sensitive.
-	*********
-	*/
-	///Numbers
-	document.write(1);
-	document.write('<br>');  /// this is just a line break
-	document.write(1.5);
-	document.write('<br>');	/// this is just a line break
-	///Strings
-	document.write('Hello');
-	document.write('<br>');	/// this is just a line break
-	document.write("Hello");
-	document.write('<br>');	/// this is just a line break
+*********
+*/
+///Numbers
+document.write(1);
+document.write('<br>');  /// this is just a line break
+document.write(1.5);
+document.write('<br>');	/// this is just a line break
+///Strings
+document.write('Hello');
+document.write('<br>');	/// this is just a line break
+document.write("Hello");
+document.write('<br>');	/// this is just a line break
 
-	///Variables
-	var myHolder = 6;
-	document.write(myHolder );
-	document.write('<br>');	/// this is just a line break
-	var myText = "hello";
-	document.write(myText);
-	document.write('<br>');	/// this is just a line break
+///Variables
+var myHolder = 6;
+document.write(myHolder );
+document.write('<br>');	/// this is just a line break
+var myText = "hello";
+document.write(myText);
+document.write('<br>');	/// this is just a line break
 
-	///Expressions
-	document.write(myHolder + myHolder );
-	document.write('<br>');	/// this is just a line break
-	document.write(myHolder * myHolder );
-	document.write('<br>');	/// this is just a line break
-	document.write(myText + ' ' + myText + ' ' + myHolder);
-	document.write('<br>');	/// this is just a line break
-    	/*
-	*********
+///Expressions
+document.write(myHolder + myHolder );
+document.write('<br>');	/// this is just a line break
+document.write(myHolder * myHolder );
+document.write('<br>');	/// this is just a line break
+document.write(myText + ' ' + myText + ' ' + myHolder);
+document.write('<br>');	/// this is just a line break
+/*
+*********
 
-	*********
-	*/
-	///Numbers
-	document.write(1);
-	document.write('<br>');  /// this is just a line break
-	document.write(1.5);
-	document.write('<br>');	/// this is just a line break
-	///Strings
-	document.write('Hello');
-	document.write('<br>');	/// this is just a line break
-	document.write("Hello");
-	document.write('<br>');	/// this is just a line break
+*********
+*/
+///Numbers
+document.write(1);
+document.write('<br>');  /// this is just a line break
+document.write(1.5);
+document.write('<br>');	/// this is just a line break
+///Strings
+document.write('Hello');
+document.write('<br>');	/// this is just a line break
+document.write("Hello");
+document.write('<br>');	/// this is just a line break
 
-	///Variables
-	var myHolder = 6;
-	document.write(myHolder );
-	document.write('<br>');	/// this is just a line break
-	var myText = "hello";
-	document.write(myText);
-	document.write('<br>');	/// this is just a line break
+///Variables
+var myHolder = 6;
+document.write(myHolder );
+document.write('<br>');	/// this is just a line break
+var myText = "hello";
+document.write(myText);
+document.write('<br>');	/// this is just a line break
 
-	///Expressions
-	document.write(myHolder + myHolder );
-	document.write('<br>');	/// this is just a line break
-	document.write(myHolder * myHolder );
-	document.write('<br>');	/// this is just a line break
-	document.write(myText + ' ' + myText + ' ' + myHolder);
-	document.write('<br>');	/// this is just a line break
+///Expressions
+document.write(myHolder + myHolder );
+document.write('<br>');	/// this is just a line break
+document.write(myHolder * myHolder );
+document.write('<br>');	/// this is just a line break
+document.write(myText + ' ' + myText + ' ' + myHolder);
+document.write('<br>');	/// this is just a line break
 
 ///Number
 var myNumber = 100;
@@ -188,37 +188,37 @@ document.write('<br>');	/// this is just a line break
 document.write(parseFloat(myString));
 document.write('<br>');	/// this is just a line break
 ///Working with Numbers
-	var myNumber = 100.1234;
-	document.write(Math.random()*11+10);
-	document.write('<br>');	/// this is just a line break
+var myNumber = 100.1234;
+document.write(Math.random()*11+10);
+document.write('<br>');	/// this is just a line break
 
-	document.write(Math.round(myNumber));
-	document.write('<br>');	/// this is just a line break
+document.write(Math.round(myNumber));
+document.write('<br>');	/// this is just a line break
 
-	document.write(Math.ceil(myNumber));
-	document.write('<br>');	/// this is just a line break
-	///Dates
-	document.write(Date());
-	document.write('<br>');	/// this is just a line break
+document.write(Math.ceil(myNumber));
+document.write('<br>');	/// this is just a line break
+///Dates
+document.write(Date());
+document.write('<br>');	/// this is just a line break
 
-	document.write(new Date(2014,11,31,5,10,30,0));
-	document.write('<br>');	/// this is just a line break
+document.write(new Date(2014,11,31,5,10,30,0));
+document.write('<br>');	/// this is just a line break
 
-	document.write(new Date("2014,DECEMBER,31"));
-	document.write('<br>');	/// this is just a line break
+document.write(new Date("2014,DECEMBER,31"));
+document.write('<br>');	/// this is just a line break
 
-	document.write(new Date().getTime());
-	document.write('<br>');	/// this is just a line break
+document.write(new Date().getTime());
+document.write('<br>');	/// this is just a line break
 
-	document.write(new Date().getHours());
-	document.write('<br>');	/// this is just a line break
+document.write(new Date().getHours());
+document.write('<br>');	/// this is just a line break
 ///Regular Expressions
-	var needle = /findme/i;
-	var myText = "if you can findme and replaceme then  this really works";
+var needle = /findme/i;
+var myText = "if you can findme and replaceme then  this really works";
 
-	document.write(myText.search(needle));
-	document.write('<br>');	/// this is just a line break
+document.write(myText.search(needle));
+document.write('<br>');	/// this is just a line break
 
-	var newText  = myText.replace(" this", "me");
-	document.write(newText);
-	document.write('<br>');	/// this is just a line break
+var newText  = myText.replace(" this", "me");
+document.write(newText);
+document.write('<br>');	/// this is just a line break
